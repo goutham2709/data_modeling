1952a073acd5d83ea4999ab2af87daeb2a38bdee
GOUTHAM <eversmilinggoutham@gmail.com>
2020-05-07 11:46:17 -0400

    Split the data into test and training sets with Simple random sampling
    Updated script for simple random sampling
    
    saved train and test sets into RData objects

--- Simple random sampling.R
+++ Simple random sampling.R
@@ -2,23 +2,15 @@
     # Partitioning the dataset into Training and validation sets.
           # Similar to "Validation set approach."
 
-input_data <- readline(prompt="Enter input data name: ")
-
-# print(paste("Hi,", my.name, "next year you will be", my.age+1, "years old."))
-
-
-
-
-
-train_size <- floor(0.75 * nrow(input_data)) # Set size of training dataset
+train_size <- floor(0.75 * nrow(DATA)) # Set size of training dataset
 set.seed(X)
-train <- sample(seq_len(nrow(input_data)), size = train_size) # Generates random numbers
+train <- sample(seq_len(nrow(DATA)), size = train_size) # Generates random numbers
 
 train_set <- DATA[train, ]
 test_set <- DATA[-train, ]
 
 
 # Same logic, simplified code
-train_set <- input_data[sample(nrow(input_data), size = X)]
-test_set <- input_data[-sample(nrow(input_data), size = X)]
+train_set <- DATA[sample(nrow(DATA), size = X)]
+test_set <- DATA[-sample(nrow(DATA), size = X)]
 
