e413417fe748bacf347fdc84b1b201e54d3ac12f
GOUTHAM <eversmilinggoutham@gmail.com>
2020-05-08 22:03:16 -0400

    end of day 2

--- Outlier_treatment.R
+++ Outlier_treatment.R
@@ -3,3 +3,17 @@
 ## Outlier treatment
 
 # Checking
+DATA[DATA$COL > 1.5*IQR+Q3,] # Gives outliers above upper limit
+DATA[DATA$COL > 1.5*IQR+Q1,] # Gives outliers below lower limit
+boxplot(DATA$COL)
+
+
+# Function to iterate through each column and check output boxplots
+outlier_func <- function(data){
+  columns <- colnames(data)
+  for(val in columns){
+    if(is.factor(val)){
+      boxplot(data$val)
+    }
+  }
+}
